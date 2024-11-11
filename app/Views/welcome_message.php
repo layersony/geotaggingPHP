e13c0cba8d3ca176f72f0826259a14ce90629d4d
Maingi <44671255+layersony@users.noreply.github.com>
2024-11-11 10:57:20 +0300

    Update welcome_message.php
    Fix: update script to use api.opencagedata

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -85,13 +85,20 @@
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(
                 (position) => {
-                    locationContainer.style.display = "none";
+                    // locationContainer.style.display = "none";
                     autofillLocation(); // this is a temporary workaround
 
                     
-                    // const latitude = position.coords.latitude;
-                    // const longitude = position.coords.longitude;
+                    const latitude = position.coords.latitude;
+                    const longitude = position.coords.longitude;
                     // could use OpenCage Geocoder or Nominatim to convert the coordinates to a location name
+                    // Key will be hidden (Just for Testing Purposes Only will be deactivated soon)
+                    fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}%2C${longitude}&key=f2243577fa5046f9800423398c7d8a50`)
+                    .then(response => response.json())
+                    .then(resData => {
+                        const city = data['results']['components']['city']
+                        document.getElementById("location").value = city;
+                    })
                     
                 },
                 (error) => {
@@ -116,6 +123,160 @@
                 console.error("Error fetching IP-based location:", error);
             });
     }
+
+    // sample opencagedata
+    //{
+    //   "documentation": "https://opencagedata.com/api",
+    //   "licenses": [
+    //     {
+    //       "name": "see attribution guide",
+    //       "url": "https://opencagedata.com/credits"
+    //     }
+    //   ],
+    //   "rate": {
+    //     "limit": 2500,
+    //     "remaining": 2497,
+    //     "reset": 1731369600
+    //   },
+    //   "results": [
+    //     {
+    //       "annotations": {
+    //         "DMS": {
+    //           "lat": "1° 18' 1.76004'' S",
+    //           "lng": "36° 47' 2.24052'' E"
+    //         },
+    //         "MGRS": "37MBU5341156148",
+    //         "Maidenhead": "KI88jq47bv",
+    //         "Mercator": {
+    //           "x": 4094771.216,
+    //           "y": -143813.133
+    //         },
+    //         "OSM": {
+    //           "edit_url": "https://www.openstreetmap.org/edit?way=561265502#map=17/-1.30049/36.78396",
+    //           "note_url": "https://www.openstreetmap.org/note/new#map=17/-1.30049/36.78396&layers=N",
+    //           "url": "https://www.openstreetmap.org/?mlat=-1.30049&mlon=36.78396#map=17/-1.30049/36.78396"
+    //         },
+    //         "UN_M49": {
+    //           "regions": {
+    //             "AFRICA": "002",
+    //             "EASTERN_AFRICA": "014",
+    //             "KE": "404",
+    //             "SUB-SAHARAN_AFRICA": "202",
+    //             "WORLD": "001"
+    //           },
+    //           "statistical_groupings": [
+    //             "LEDC"
+    //           ]
+    //         },
+    //         "callingcode": 254,
+    //         "currency": {
+    //           "alternate_symbols": [
+    //             "Sh"
+    //           ],
+    //           "decimal_mark": ".",
+    //           "html_entity": "",
+    //           "iso_code": "KES",
+    //           "iso_numeric": "404",
+    //           "name": "Kenyan Shilling",
+    //           "smallest_denomination": 50,
+    //           "subunit": "Cent",
+    //           "subunit_to_unit": 100,
+    //           "symbol": "KSh",
+    //           "symbol_first": 1,
+    //           "thousands_separator": ","
+    //         },
+    //         "flag": "🇰🇪",
+    //         "geohash": "kzf0t5320733d2w7x5ne",
+    //         "qibla": 7.29,
+    //         "roadinfo": {
+    //           "drive_on": "left",
+    //           "road": "unnamed road",
+    //           "road_type": "residential",
+    //           "speed_in": "km/h"
+    //         },
+    //         "sun": {
+    //           "rise": {
+    //             "apparent": 1731294780,
+    //             "astronomical": 1731290460,
+    //             "civil": 1731293460,
+    //             "nautical": 1731291960
+    //           },
+    //           "set": {
+    //             "apparent": 1731338460,
+    //             "astronomical": 1731342780,
+    //             "civil": 1731339780,
+    //             "nautical": 1731341280
+    //           }
+    //         },
+    //         "timezone": {
+    //           "name": "Africa/Nairobi",
+    //           "now_in_dst": 0,
+    //           "offset_sec": 10800,
+    //           "offset_string": "+0300",
+    //           "short_name": "EAT"
+    //         },
+    //         "what3words": {
+    //           "words": "juggled.zaps.leader"
+    //         }
+    //       },
+    //       "bounds": {
+    //         "northeast": {
+    //           "lat": -1.299805,
+    //           "lng": 36.7839757
+    //         },
+    //         "southwest": {
+    //           "lat": -1.3013212,
+    //           "lng": 36.7839378
+    //         }
+    //       },
+    //       "components": {
+    //         "ISO_3166-1_alpha-2": "KE",
+    //         "ISO_3166-1_alpha-3": "KEN",
+    //         "ISO_3166-2": [
+    //           "KE-30"
+    //         ],
+    //         "_category": "road",
+    //         "_normalized_city": "Nairobi",
+    //         "_type": "road",
+    //         "city": "Nairobi",
+    //         "city_block": "Kilimani sublocation",
+    //         "city_district": "Kibra",
+    //         "continent": "Africa",
+    //         "country": "Kenya",
+    //         "country_code": "ke",
+    //         "postcode": "44847",
+    //         "quarter": "Adam's Arcade",
+    //         "road": "unnamed road",
+    //         "road_type": "residential",
+    //         "state": "Nairobi County",
+    //         "suburb": "Woodley/Kenyatta/Golf Course ward"
+    //       },
+    //       "confidence": 9,
+    //       "distance_from_q": {
+    //         "meters": 11
+    //       },
+    //       "formatted": "unnamed road, Nairobi, 44847, Kenya",
+    //       "geometry": {
+    //         "lat": -1.3004889,
+    //         "lng": 36.7839557
+    //       }
+    //     }
+    //   ],
+    //   "status": {
+    //     "code": 200,
+    //     "message": "OK"
+    //   },
+    //   "stay_informed": {
+    //     "blog": "https://blog.opencagedata.com",
+    //     "mastodon": "https://en.osm.town/@opencage"
+    //   },
+    //   "thanks": "For using an OpenCage API",
+    //   "timestamp": {
+    //     "created_http": "Mon, 11 Nov 2024 07:48:19 GMT",
+    //     "created_unix": 1731311299
+    //   },
+    //   "total_results": 1
+    // }
 
     window.onload = getLocation;
 </script>
